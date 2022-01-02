ce9d0ffb247b4e39576599316ade2d3ef19de14a
0923kdh <0923kdh@gmail.com>
2022-01-02 22:48:09 +0900

    feat : predicateReduceTest

--- stream/app/src/test/java/stream/FilteringTest.java
+++ stream/app/src/test/java/stream/FilteringTest.java
@@ -2,8 +2,10 @@ package stream;
 
 import dummy.Fruits;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 public class FilteringTest {
@@ -51,6 +53,27 @@ public class FilteringTest {
     fruits.stream()
       .filter(matchNameStartsWith.or(isOver).negate())
       .forEach(System.out::println);
+  }
+
+  @Test
+  public void predicateReduceTest(){
+    List<String> firstNames = Arrays.asList("dahee", "dam", "minjin", "heehee", "min");
+    List<Predicate<String>> allPredicates = new ArrayList<Predicate<String>>();
+    allPredicates.add(str -> str.startsWith("d"));
+    allPredicates.add(str -> str.contains("h"));
+    allPredicates.add(str -> str.length() > 4);
+
+    List<String> result1 = firstNames.stream()
+      .filter(allPredicates.stream().reduce(x->true, Predicate::and))
+      .collect(Collectors.toList());
+
+    List<String> result2 = firstNames.stream()
+      .filter(allPredicates.stream().reduce(x->false, Predicate::or))
+      .collect(Collectors.toList());
+
+    result1.forEach(System.out::println);
+    System.out.println("------");
+    result2.forEach(System.out::println);
   }
 
 }
